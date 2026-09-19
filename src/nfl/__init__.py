ae20b22bb4757053ba38b45aeeeae13541e2dbb5
JacZerin24 <jaczerin24@gmail.com>
2026-09-18 22:55:20 -0500

    Create isolated NFL research package

---
+++ src/nfl/__init__.py
@@ -0,0 +1,5 @@
+"""NFL weather-totals research package.
+
+This package is intentionally isolated from the existing CFB production code.
+Nothing in here is imported by the current college-football workflow.
+"""
